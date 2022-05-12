be90c1f9ac50af9f5a6b88d34f5e2636566d935e
sepehrhashtroudi@gamil.com <sepehrhashtroudi@gmail.com>
2022-05-12 16:27:58 -0600

    runner

---
+++ Code-Code/code-refinement/code/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --job-name=Xrun # single job name for the array
+#SBATCH --time=10:00:00 # maximum walltime per job
+#SBATCH --mem=25G # maximum 100M per job
+#SBATCH --cpus-per-task=2
+#SBATCH --output=%x.out # standard output
+#SBATCH --error=%x.err # standard error
+# in the previous two lines %A" is replaced by job
+
+cd /home/arminz/CodeXGLUE-DA/Code-Code/code-refinement/code
+source env/bin/activate
+
+python run.py \
+	--do_train \
+	--do_eval \
+	--model_type roberta \
+	--model_name_or_path microsoft/codebert-base \
+	--config_name roberta-base \
+	--tokenizer_name roberta-base \
+	--train_filename ../data/small/train.buggy-fixed.buggy,../data/small/train.buggy-fixed.fixed \
+	--dev_filename ../data/small/valid.buggy-fixed.buggy,../data/small/valid.buggy-fixed.fixed \
+	--output_dir ./output \
+	--max_source_length 256 \
+	--max_target_length 256 \
+	--beam_size 5 \
+	--train_batch_size 64 \
+	--eval_batch_size 64 \
+	--learning_rate 5e-5 \
+	--train_steps 100000 \
+	--eval_steps 5000
