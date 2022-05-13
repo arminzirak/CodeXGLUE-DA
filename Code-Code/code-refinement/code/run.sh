9314f75d3dde4bc87cef1439237454cd92007731
sepehrhashtroudi@gamil.com <sepehrhashtroudi@gmail.com>
2022-05-13 09:46:45 -0600

    runner address and batch size fixes

--- Code-Code/code-refinement/code/run.sh
+++ Code-Code/code-refinement/code/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --gres=gpu:v100l:1
-#SBATCH --job-name=Xrun # single job name for the array
+#SBATCH --job-name=Xruns # single job name for the array
 #SBATCH --time=10:00:00 # maximum walltime per job
 #SBATCH --mem=25G # maximum 100M per job
 #SBATCH --cpus-per-task=2
@@ -20,12 +20,12 @@ python run.py \
 	--tokenizer_name roberta-base \
 	--train_filename ../data/small/train.buggy-fixed.buggy,../data/small/train.buggy-fixed.fixed \
 	--dev_filename ../data/small/valid.buggy-fixed.buggy,../data/small/valid.buggy-fixed.fixed \
-	--output_dir ./output \
+	--output_dir ~/scratch/Xoutputs \
 	--max_source_length 256 \
 	--max_target_length 256 \
 	--beam_size 5 \
-	--train_batch_size 64 \
-	--eval_batch_size 64 \
+	--train_batch_size 32 \
+	--eval_batch_size 32 \
 	--learning_rate 5e-5 \
 	--train_steps 100000 \
 	--eval_steps 5000
