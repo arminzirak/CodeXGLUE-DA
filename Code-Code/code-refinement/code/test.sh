fde6b50f0fafc0293bebcf9e294ce864ee13412f
sepehrhashtroudi@gamil.com <sepehrhashtroudi@gmail.com>
2022-05-13 13:51:28 -0600

    train source

--- Code-Code/code-refinement/code/test.sh
+++ Code-Code/code-refinement/code/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --gres=gpu:v100l:1
-#SBATCH --job-name=XTss # single job name for the array
-#SBATCH --time=60:00:00 # maximum walltime per job
+#SBATCH --job-name=Xtests # single job name for the array
+#SBATCH --time=2:00:00 # maximum walltime per job
 #SBATCH --mem=25G # maximum 100M per job
 #SBATCH --cpus-per-task=2
 #SBATCH --output=%x.out # standard output
@@ -12,14 +12,14 @@ cd /home/arminz/CodeXGLUE-DA/Code-Code/code-refinement/code
 source env/bin/activate
 
 python run.py \
-	--do_train \
-	--do_eval \
+	--do_test \
+	--output_dir ./test-output \
+	--load_model_path ./output/checkpoint-best-ppl/pytorch_model.bin \
 	--model_type roberta \
-	--model_name_or_path /home/arminz/codebert-base \
+	--model_name_or_path microsoft/codebert-base \
 	--config_name roberta-base \
 	--tokenizer_name roberta-base \
-	--data_dir ../data/datasets/50/ \
-	--output_dir ~/scratch/Xoutputs/train_source \
+	--data_dir 	../data/datasets/50/ \
 	--max_source_length 256 \
 	--max_target_length 256 \
 	--beam_size 5 \
