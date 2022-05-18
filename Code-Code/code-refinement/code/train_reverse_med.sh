c15ffb495438e46cae9b4b4e6d26ed259d8082ed
Armin <armin.zirak97@gmail.com>
2022-05-17 18:00:31 -0600

    medium scripts

---
+++ Code-Code/code-refinement/code/train_reverse_med.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --job-name=XRtrM # single job name for the array
+#SBATCH --time=60:00:00 # maximum walltime per job
+#SBATCH --mem=25G # maximum 100M per job
+#SBATCH --cpus-per-task=2
+#SBATCH --output=%x.out # standard output
+#SBATCH --error=%x.err # standard error
+# in the previous two lines %A" is replaced by job
+
+cd /home/arminz/CodeXGLUE-DA/Code-Code/code-refinement/code
+source env/bin/activate
+
+python run_reverse.py \
+	--do_train \
+	--do_eval \
+	--model_type roberta \
+	--model_name_or_path /home/arminz/codebert-base \
+	--config_name roberta-base \
+	--tokenizer_name roberta-base \
+	--data_dir ../data/datasets/50-100/ \
+	--output_dir ~/scratch/Xoutputs/50-100/reverse/train_source \
+	--max_source_length 256 \
+	--max_target_length 256 \
+	--beam_size 5 \
+	--train_batch_size 32 \
+	--eval_batch_size 32 \
+	--learning_rate 5e-5 \
+	--train_steps 100000 \
+	--eval_steps 5000 \
+	--domains source \
